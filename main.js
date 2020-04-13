21ad97b9a8ad477391c5865d2a6a73ac68ef1fac
Sam Liman <myohygiene@gmail.com>
2020-04-13 12:42:46 -0600

    added  color on hover to about font awesome and image links

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+if (typeof jQuery == 'undefined'){
+    console.log('oops! I still have to link my jQuery properly!');
+  } else {console.log('I did it! I linked jQuery and this js file!')};
+
 $(document).ready(function () {
     $('.menu-toggler').on('click', function () {
         $(this).toggleClass('open');
