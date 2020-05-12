db56f1cf382f36ce031f64c513927250a8b86482
Sam Liman <myohygiene@gmail.com>
2020-05-12 14:01:50 -0600

    adding responsive features

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-if (typeof jQuery == 'undefined'){
-    console.log('oops! I still have to link my jQuery properly!');
-  } else {console.log('I did it! I linked jQuery and this js file!')};
+// if (typeof jQuery == 'undefined') {
+//     console.log('oops! I still have to link my jQuery properly!');
+// } else { console.log('I did it! I linked jQuery and this js file!') };
 
 $(document).ready(function () {
     $('.menu-toggler').on('click', function () {
@@ -11,5 +11,17 @@ $(document).ready(function () {
     $('.top-nav .nav-link').on('click', function () {
         $('.menu-toggler').removeClass('open');
         $('.top-nav').removeClass('open');
+    });
+
+    $('nav a[href*="#"]').on('click', function () {
+        $('html, body').animate( keyframes, {
+            scrollTop: $($(this).attr('href')).offset().top - 100
+        }, options, 2000);
+    });
+
+    $('#up').on('click', function () {
+        $('#up').animate( keyframes, {
+            scrollTop: 0
+        }, options, 2000);
     });
 });
